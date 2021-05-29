1e377cb7cd79c6e7d77b605066efd61c6ec955d3
laylanrr <85001389+laylanrr@users.noreply.github.com>
2021-05-29 10:43:42 +0700

    Add files via upload

---
+++ splashscreen.java
@@ -0,0 +1,21 @@
+package com.example.lae;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+    }
+}
